0f6aa19d8d83163452aa8eae7ec4e4968730b121
KimM-09 <kmmclan09@gmail.com>
2026-01-18 21:53:56 -0500

    Cart quantity tracking, button disabled after adding to cart, fixed issue with image in product list

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
@@ -264,7 +264,15 @@ function ProductList({ onHomeClick }) {
         setAddedToCart((prevState) => ({
             ...prevState, [item.name]: true,
         }));
+
     };
+
+    const calculateTotalQuantity = () => {
+        const CartItems = useSelector(state => state.cart.items)
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    }
+        
+    
 
     return (
         <div>
@@ -283,7 +291,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h3 className='cart'>{calculateTotalQuantity()}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h3></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -295,16 +303,16 @@ function ProductList({ onHomeClick }) {
                         {item.plants.map((plant, plantIndex) => (
                             <div className='product-card' key={plantIndex}>
                                 <h3 className='product-title'>{plant.name}</h3>
-                                <image className='product-image' src={plant.image} alt={plant.name} />
+                                <img className='product-image' src={plant.image} alt={plant.name} />
                                 <p>{plant.description}</p>
                                 <p className='product-price'>{plant.cost}</p>
-                                <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`} onClick={() => handleAddToCart(plant)} disabled={addedToCart[plant.name]}>Add to Cart</button>
                             </div>
                         ))}
                         </div>
                         </div>
                     ))}
-
+ 
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
